4f8732259f6979c666737e16e1f617213a85d7af
Ivan <tornadod@mail.ru>
2022-08-17 21:09:29 +0300

    промежуточный

---
+++ ASP_Net_GB_lesson1/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_Net_GB_lesson1
+{
+    internal class Post
+    {
+        private int UserId;
+        private int Id;
+        private string Title;
+        private string Body;
+
+        public Post(int userId,int id,string title, string body )
+        {
+            UserId = userId;
+            Id = id;
+            Title = title;
+            Body = body;
+        }
+    }
+}
